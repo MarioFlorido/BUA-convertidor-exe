81e046035839b143daa1035730dd5bc53a608a24
Mario Florido Pérez <mariofloridoperez@iMac-de-PuntBIU.local>
2026-06-01 13:46:25 +0200

    feat(elpx): añadir pp_organization (Biblioteca Universitaria) a propiedades del proyecto

--- src/core/renderers/ElpxRenderer.ts
+++ src/core/renderers/ElpxRenderer.ts
@@ -84,6 +84,7 @@ export class ElpxRenderer {
   <odeProperty><key>pp_title</key><value>${escapeXml(this.project.title || 'Documento importado')}</value></odeProperty>
   <odeProperty><key>pp_subtitle</key><value>${escapeXml(this.project.subtitle || '')}</value></odeProperty>
   <odeProperty><key>pp_author</key><value>Biblioteca de la Universidad de Alicante</value></odeProperty>
+  <odeProperty><key>pp_organization</key><value>Biblioteca de la Universidad de Alicante</value></odeProperty>
   <odeProperty><key>pp_lang</key><value>es</value></odeProperty>
   <odeProperty><key>pp_license</key><value>creative commons: attribution - non commercial - share alike 4.0</value></odeProperty>
   <odeProperty><key>pp_licenseUrl</key><value>https://creativecommons.org/licenses/by-nc-sa/4.0/</value></odeProperty>
